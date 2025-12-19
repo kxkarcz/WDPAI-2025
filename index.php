76d0d3da5efd4e545622ba6c41d881b74e1d080e
kxkarcz <76162499+kxkarcz@users.noreply.github.com>
2025-12-19 07:55:06 +0100

    fix index.php

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
-require_once 'Routing.php';
 
-$path = trim($_SERVER['REQUEST_URI'], '/');
-$path = parse_url($path, PHP_URL_PATH);
+declare(strict_types=1);
 
-Routing::run($path);
-
-
+require __DIR__ . '/public/index.php';
